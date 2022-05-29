0923655a34d1481905839bf9615d6dba6b7aa67f
jennie267 <jennieee267@gmail.com>
2022-05-29 23:24:46 +0900

    refactor : UserDto로 리턴하도록 수정 (불필요한 필드 안보이게하려고 ex password, isActive)

--- src/main/java/study/cherry/jwttutorial/dto/UserDto.java
+++ src/main/java/study/cherry/jwttutorial/dto/UserDto.java
@@ -2,9 +2,12 @@ package study.cherry.jwttutorial.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import study.cherry.jwttutorial.entity.User;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -25,5 +28,19 @@ public class UserDto {
     @NotNull
     @Size(min = 3, max = 50)
     private String nickname;
+
+    private Set<AuthorityDto> authorityDtoSet;
+
+    public static UserDto from(User user) {
+        if(user == null) return null;
+
+        return UserDto.builder()
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .authorityDtoSet(user.getAuthorities().stream()
+                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+    }
 
 }
--- src/main/java/study/cherry/jwttutorial/service/UserService.java
+++ src/main/java/study/cherry/jwttutorial/service/UserService.java
@@ -1,9 +1,6 @@
 package study.cherry.jwttutorial.service;
 
 
-import java.util.Collections;
-import java.util.Optional;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +9,8 @@ import study.cherry.jwttutorial.entity.Authority;
 import study.cherry.jwttutorial.entity.User;
 import study.cherry.jwttutorial.repository.UserRepository;
 import study.cherry.jwttutorial.util.SecurityUtil;
+
+import java.util.Collections;
 
 @Service
 public class UserService {
@@ -24,7 +23,7 @@ public class UserService {
     }
 
     @Transactional
-    public User signup(UserDto userDto) {
+    public UserDto signup(UserDto userDto) {
         if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
             throw new RuntimeException("이미 가입되어 있는 유저입니다.");
         }
@@ -41,16 +40,16 @@ public class UserService {
                 .activated(true)
                 .build();
 
-        return userRepository.save(user);
+        return UserDto.from(userRepository.save(user));
     }
 
     @Transactional(readOnly = true)
-    public Optional<User> getUserWithAuthorities(String username) {
-        return userRepository.findOneWithAuthoritiesByUsername(username);
+    public UserDto getUserWithAuthorities(String username) {
+        return UserDto.from(userRepository.findOneWithAuthoritiesByUsername(username).orElse(null));
     }
 
     @Transactional(readOnly = true)
-    public Optional<User> getMyUserWithAuthorities() {
-        return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
+    public UserDto getMyUserWithAuthorities() {
+        return UserDto.from(SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername).orElse(null));
     }
 }
