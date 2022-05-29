1b7316e2bfe086a54c8e70b32b5fd8345d84697d
jennie267 <jennieee267@gmail.com>
2022-05-29 23:25:09 +0900

    style : import 정리

--- src/main/java/study/cherry/jwttutorial/controller/UserController.java
+++ src/main/java/study/cherry/jwttutorial/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import study.cherry.jwttutorial.dto.UserDto;
-import study.cherry.jwttutorial.entity.User;
 import study.cherry.jwttutorial.service.UserService;
 
 import javax.validation.Valid;
